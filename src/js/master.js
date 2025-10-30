e7959f77fa33de3a74d26bdc1cca8f4cf51c593b
parsa <parsafarashah@gmail.com>
2025-10-30 16:25:36 +0330

    add style question

--- src/js/master.js
+++ src/js/master.js
@@ -14,10 +14,34 @@ $startBtn.addEventListener("click", () => {
       items.map((item) => {
         let _div = document.createElement("div");
         _div.classList.add("px-4");
+        _div.classList.add("w-full");
+        _div.classList.add("bg-orange-400");
+        _div.classList.add("h-full");
+        _div.classList.add("py-7");
+        _div.classList.add("bg-white");
         _div.innerHTML = `
         <div class="flex items-center gap-4">
-       <span class="text-2xl">(${item.id})</span> <h1>${item.question}</h1>
-        </div>
+              <span class="text-2xl">(${item.id})</span>
+              <h1>${item.question}</h1>
+            </div>
+
+            <div class="rounded-2xl">
+              <p class="border border-[#61616157] px-5 rounded-2xl mt-4 bg-[#c5c5c5b0] py-2 cursor-pointer hover:bg-[#e6e6e6fb] duration-300">
+                الف - ${item.options[0]}
+              </p>
+              <p class="border px-5 rounded-2xl mt-4 bg-[#c5c5c5b0] py-2 cursor-pointer hover:bg-[#e6e6e6fb] duration-300">ب - ${item.options[1]}</p>
+              <p class="border px-5 rounded-2xl mt-4 bg-[#c5c5c5b0] py-2 cursor-pointer hover:bg-[#e6e6e6fb] duration-300">ج - ${item.options[2]}</p>
+              <p class="border px-5 rounded-2xl mt-4 bg-[#c5c5c5b0] py-2 cursor-pointer hover:bg-[#e6e6e6fb] duration-300">د - ${item.options[3]}</p>
+            </div>
+            <div class="w-full flex justify-center items-center mt-6">
+              <button
+                onclick="btnAwnser()"
+                class="px-5 py-2 bg-green-500 rounded-md"
+              >
+                ثبت جواب
+              </button>
+            </div>
+
         `;
         $questionBox.appendChild(_div);
       });
